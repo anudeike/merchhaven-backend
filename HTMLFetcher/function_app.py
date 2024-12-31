659ff6f3ce748f7fe809c918c5f70ce7742feab2
Ikechukwu <31142429+anudeike@users.noreply.github.com>
2024-12-31 09:46:39 -0500

    added the html parser function

---
+++ HTMLFetcher/function_app.py
@@ -0,0 +1,10 @@
+import azure.functions as func
+import logging
+
+app = func.FunctionApp()
+
+@app.queue_trigger(arg_name="azqueue", queue_name="urlqueue",
+                               connection="b0e0f3_STORAGE") 
+def HTMLFetcherFunc(azqueue: func.QueueMessage):
+    logging.info('Python Queue trigger processed a message: %s',
+                azqueue.get_body().decode('utf-8'))
